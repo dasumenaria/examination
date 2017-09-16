72acdef6da00daba9a03348922e00633d5de702d
Gopesh Parihar <gopeshparihar7@gmail.com>
2017-09-16 12:04:30 +0530

    Module Table

--- database/modules (1).sql
+++ database/modules (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 31, 2017 at 07:00 AM
+-- Generation Time: Sep 16, 2017 at 08:32 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `exam_2017-2018`
+-- Database: `exam_udaipur_2017-2018`
 --
 
 -- --------------------------------------------------------
@@ -48,21 +48,21 @@ INSERT INTO `modules` (`id`, `name`, `main_menu`, `main_menu_icon`, `sub_menu`, 
 (1, ' Dashboard', '', '', '', '', 'index.php', 'fa fa-home', '', '', 1),
 (2, ' Add Section', 'Master', 'fa fa-book', '', '', 'section.php', 'fa fa-search-plus', '', '', 3),
 (3, ' Section-Mapping', 'Master', 'fa fa-book', '', '', 'cls_sec.php', 'fa fa-edit', '', '', 4),
-(4, ' Subject-Mapping', 'Master', 'fa fa-book', '', '', 'cls_sub.php', 'fa fa-book ', '', '', 6),
-(5, ' Assign Max Marks', 'Master', 'fa fa-book', '', '', 'max_marks.php', 'fa fa-male', '', '', 11),
-(7, ' Assign-Elective', 'Master', 'fa fa-book', '', '', 'elective.php', 'fa fa-bar-chart-o', '', '', 12),
-(8, ' Add', ' Student Register', 'fa fa-child', '', '', 'student_reg.php', 'fa fa-user', '', '', 4),
+(4, ' Subject-Mapping', 'Master', 'fa fa-book', '', '', 'cls_sub.php', 'fa fa-book ', '', '', 8),
+(5, ' Assign Max Marks', 'Master', 'fa fa-book', '', '', 'max_marks.php', 'fa fa-male', '', '', 14),
+(7, ' Assign-Elective', 'Master', 'fa fa-book', '', '', 'elective.php', 'fa fa-bar-chart-o', '', '', 9),
+(8, ' Add', 'Student Register', 'fa fa-child', '', '', 'student_reg.php', 'fa fa-user', '', '', 4),
 (9, ' Download Excel', ' Marks', 'fa fa-edit', '', '', 'marks.php', 'fa fa-picture-o ', '', '', 3),
 (10, 'Upload Excel', ' Marks', 'fa fa-edit', '', '', 'upload_marks.php', 'fa fa-thumbs-o-up ', '', '', 4),
 (11, 'Marksheet View', ' Marks', 'fa fa-edit', '', '', 'marksheet.php', 'fa fa-picture-o ', '', '', 5),
 (12, ' Edit Class Wise', ' Marks', 'fa fa-edit', '', '', 'number_view.php', 'fa fa-ticket ', '', '', 6),
-(15, 'Activate Exam', 'Master', 'fa fa-book', '', '', 'term_block.php', 'fa fa-eye ', '', '', 13),
+(15, 'Activate Exam', 'Master', 'fa fa-book', '', '', 'term_block.php', 'fa fa-eye ', '', '', 17),
 (16, ' Add', ' Faculty', 'fa fa-group', '', '', 'register.php', 'fa \nfa-check-square-o ', '', '', 5),
 (18, 'View', ' Faculty', 'fa fa-group', '', '', 'staff_view.php', 'fa fa-level-up', '', '', 6),
 (19, ' Password', ' Settings', 'fa fa-gears', '', '', 'password.php', 'fa fa-lock', '', '', 6),
 (20, 'User Privileges', ' Settings', 'fa fa-gears', '', '', 'user.php', '', '', '', 7),
-(21, 'Exam Mapping', 'Master', 'fa fa-book', '', '', 'assign_mark.php', '', '', '', 10),
-(22, ' View', ' Student Register', 'fa fa-child', '', '', 'student_updt.php', 'fa fa-book', '', '', 5),
+(21, 'Exam Mapping', 'Master', 'fa fa-book', '', '', 'assign_mark.php', '', '', '', 13),
+(22, ' View', 'Student Register', 'fa fa-child', '', '', 'student_updt.php', 'fa fa-book', '', '', 5),
 (23, ' Edit Subject Wise', ' Marks', 'fa fa-edit', '', '', 'number_sub_view.php', 'fa fa-edit', '', '', 7),
 (28, 'Class Wise', 'Report', 'fa fa-file', '', '', 'report_view.php', '', '', '', 11),
 (29, 'Subject Wise', 'Report', 'fa fa-file', '', '', 'report_sub_view.php', '', '', '', 12),
@@ -73,25 +73,28 @@ INSERT INTO `modules` (`id`, `name`, `main_menu`, `main_menu_icon`, `sub_menu`, 
 (38, ' Class Wise', 'Remove', 'fa fa-thumbs-o-down ', '', '', 'remove.php', 'fa fa-cross', '', '', 9),
 (39, ' Result', '', '', '', '', 'status.php', 'fa fa-thumbs-o-up ', '', '', 10),
 (40, ' Subject Wise', 'Remove', 'fa fa-thumbs-o-down ', '', '', 'remove_sub.php', 'fa fa-danger', '', '', 10),
-(42, 'Category', 'Master', 'fa fa-book', '', '', 'master_exam_category.php', '', '', '', 7),
-(43, 'Category Type', 'Master', 'fa fa-book', '', '', 'master_exam_category_type.php', '', '', '', 8),
-(44, 'Term', 'Master', 'fa fa-book', '', '', 'master_term.php', '', '', '', 9),
-(45, 'Architecture', 'Master', 'fa fa-book', '', '', 'architecture.php', '', '', '', 16),
-(46, 'Class', 'Master', 'fa fa-book', '', '', 'master_class.php', '', '', '', 2),
-(47, 'Grade', 'Master', 'fa fa-book', '', '', 'master_grade.php', '', '', '', 20),
+(42, 'Category', 'Master', 'fa fa-book', '', '', 'master_exam_category.php', '', '', '', 11),
+(43, 'Category Type', 'Master', 'fa fa-book', '', '', 'master_exam_category_type.php', '', '', '', 12),
+(44, 'Term', 'Master', 'fa fa-book', '', '', 'master_term.php', '', '', '', 10),
+(45, 'Architecture', 'Master', 'fa fa-book', '', '', 'architecture.php', '', '', '', 15),
+(46, 'Add Class', 'Master', 'fa fa-book', '', '', 'master_class.php', '', '', '', 2),
+(47, 'Grade', 'Master', 'fa fa-book', '', '', 'master_grade.php', '', '', '', 16),
 (48, 'Subject', 'Master', 'fa fa-book', '', '', 'master_subject.php', '', '', '', 5),
 (50, 'Download', 'Health Status', 'fa fa-user-md', '', '', 'helth.php', '', '', '', 8),
 (51, 'Upload', 'Health Status', 'fa fa-user-md', '', '', 'upload_health.php', '', '', '', 9),
 (52, 'Edit', 'Health Status', 'fa fa-user-md', '', '', 'health_view.php', '', '', '', 10),
 (53, ' Delete', ' Attendance', 'fa fa-gavel', '', '', 'remove_attendance.php', 'fa fa-warning', '', '', 10),
 (54, 'Duty Charts', ' Faculty', 'fa fa-group', '', '', 'deuty_chart.php', 'fa \r\nfa-check-square-o ', '', '', 5),
-(55, 'Roll No Allocation', ' Student Register', 'fa fa-child', '', '', 'roll_no_allocation.php', 'fa fa-user', '', '', 4),
+(55, 'Roll No Allocation', 'Student Register', 'fa fa-child', '', '', 'roll_no_allocation.php', 'fa fa-user', '', '', 4),
 (56, 'Time Table', 'Report', 'fa fa-file', '', '', 'time_table.php', '', '', '', 13),
 (57, 'Roll No', 'Report', 'fa fa-file', '', '', 'roll_no.php', '', '', '', 14),
 (58, 'Seating Arrangement', 'Report', 'fa fa-file', '', '', 'seat_arang.php', '', '', '', 15),
 (59, 'Cross Class List', 'Report', 'fa fa-file', '', '', 'cross_class.php', '', '', '', 16),
 (60, 'Cross Section List', 'Report', 'fa fa-file', '', '', 'cross_section.php', '', '', '', 17),
-(61, 'Attendance', 'Report', 'fa fa-file', '', '', 'attendance_report.php', '', '', '', 18);
+(61, 'Attendance', 'Report', 'fa fa-file', '', '', 'attendance_report.php', '', '', '', 18),
+(62, 'Change section & Roll No', 'Student Register', 'fa fa-child', '', '', 'esit_student_section.php', 'fa fa-user', '', '', 4),
+(63, 'Grade Subject', 'Master', 'fa fa-book', '', '', 'grade_subject.php', '', '', '', 7),
+(64, 'Sub Subject', 'Master', 'fa fa-book', '', '', 'master_sub_subject.php', '', '', '', 6);
 
 --
 -- Indexes for dumped tables
@@ -111,7 +114,7 @@ ALTER TABLE `modules`
 -- AUTO_INCREMENT for table `modules`
 --
 ALTER TABLE `modules`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
